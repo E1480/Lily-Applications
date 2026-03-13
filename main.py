bcd3711e51a63a00dd249193381f3c008a49f408
E1480 <joviajrjulius@gmail.com>
2026-03-13 09:55:19 +0800

    Restart

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from UIComponents.ui_lib import *  # noqa: F403
-from UIComponents.ui_templates import Application  # noqa: F403
+import customtkinter as ctk
+
+def createApp() -> ctk.CTk:
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
+
+    app = ctk.CTk()
+    app.title("Lily Applications")
+    app.geometry("400x300")
+
+    return app
 
 if __name__ == "__main__":
-    app = Application(geometry="400x240", title="Lily")
+    root = createApp()
 
+
+    root.mainloop()
     
-    UIButton(app, text="Hello World").pack(pady=20) # noqa: F405    
-    UILabel(app, text="Welcome to Lily!", font=(CURRENT.FONT[0], CURRENT.FONT[1])).pack(pady=20)  # noqa: F405
-
-    app.mainloop()  
