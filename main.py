49615fbe05cb2b7478e1b27cfb4682d087887853
E1480 <joviajrjulius@gmail.com>
2026-03-13 22:34:46 +0800

    fetch

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import customtkinter as ctk
 
+class mainFrame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack(fill="both", expand=True)
+
 def createApp() -> ctk.CTk:
     ctk.set_appearance_mode("dark")
     ctk.set_default_color_theme("blue")
@@ -13,6 +18,7 @@ def createApp() -> ctk.CTk:
 if __name__ == "__main__":
     root = createApp()
 
+    mainFrame(root)
 
     root.mainloop()
     
